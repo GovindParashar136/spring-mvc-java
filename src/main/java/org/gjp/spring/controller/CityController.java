c9e7ecf5a3f2eb28159948deef3d68922ded46a7
govind.parashar <govindparashar@nhs.net>
2019-01-30 23:51:40 +0000

    update code

--- src/main/java/org/gjp/spring/controller/CityController.java
+++ src/main/java/org/gjp/spring/controller/CityController.java
@@ -7,8 +7,10 @@ import org.gjp.spring.domain.City;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test")
 public class CityController {
 
 	@GetMapping("/home")
